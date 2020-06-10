d021c3446bde7284bc7d1ecdb78158b37b068c5f
pauline_barnades <barnades.pauline@gmail.com>
2020-06-10 11:23:53 +0200

    adds and configure sequelize, adds books model with migration and seeds

---
+++ database/seeders/20200610084518-Book.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Books',
+      [
+        {
+          title: 'Learn NodeJS with Thomas',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Learn sequelize with adi',
+          createdAt: new Date(),
+          updatedAt: new Date()
+
+        }
+      ], {}
+    )
+  
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Books', null, {})
+    
+  }
+};
